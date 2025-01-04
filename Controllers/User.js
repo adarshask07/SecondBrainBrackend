1949edb6d9086329aed21eda2b041cd2358d477a
adarshask07 <119163391+adarshask07@users.noreply.github.com>
2025-01-04 20:10:27 +0530

    Update User.js

--- Controllers/User.js
+++ Controllers/User.js
@@ -87,7 +87,7 @@ export const login = async (req, res) => {
 
 export const searchContent = async (req, res) => {
     // const { query } = req.body;/
-    const query = "which is my favourite bike ?" ;
+    const query = "from what i get diseased ?" ;
     const userId = req.user._id ;
   
     try {
